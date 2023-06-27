2d9a26b6802e2cfe2626b8566f820c5932aed891
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-27 17:52:59 -0400

    Update Mission.js file

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Missions = () => <h1>Welcome to the Home page!</h1>;
+function Missions() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.spacexdata.com/v3/missions')
+      .then((res) => setData(res.data));
+  }, []);
+  return (
+    <div className="tablecontainer">
+      <div className="tablechild">
+        <table>
+          <thead>
+            <tr>
+              <th>Mission</th>
+              <th>Description</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((missions, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <tr key={index}>
+                  <td>{missions.mission_name}</td>
+                  <td>{missions.description}</td>
+                  <td>{missions.mission_id}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
 
+      </div>
+    </div>
+  );
+}
 export default Missions;
