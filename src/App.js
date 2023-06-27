865ec597b4a46d1bd418e08f9fca6c54f8482a28
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-27 10:12:44 -0400

    Fix linters and eslint errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Rockets from './components/Rockets';
+import Profile from './components/Profile';
+import Missions from './components/Mission';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Mission.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Missions = () => <h1>Welcome to the Home page!</h1>;
+
+export default Missions;
