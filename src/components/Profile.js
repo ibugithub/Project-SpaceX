867409c60411bfdfe3e8245b0bfd9733155825dc
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-29 08:39:44 -0400

    Update profile.js file

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,23 +4,18 @@ import { useSelector } from 'react-redux';
 const Profile = () => {
   const missions = useSelector((state) => state.missions);
 
-  // Filter the missions based on the `reserved` key being `true`
   const joinedMissions = missions.filter((mission) => mission.reserved);
 
   return (
     <div>
-      <h2>Joined Missions:</h2>
-      <div className="mission-list">
-        {joinedMissions.length > 0 ? (
-          <ul>
-            {joinedMissions.map((mission) => (
-              <li key={mission.mission_id}>{mission.mission_name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No joined missions found.</p>
-        )}
-      </div>
+      <h2>My Missions</h2>
+      <table>
+        {joinedMissions.map((mission) => (
+          <tr key={mission.mission_id}>
+            <td>{mission.mission_name}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
