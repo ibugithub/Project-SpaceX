d6abd67650949e8dad73b476434ccab76099a173
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-30 14:24:03 -0400

    Fis eslint errors

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,13 +1,5 @@
-// jest.config.js
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest', // Enable Babel to transform JS files
   },
-  moduleNameMapper: {
-    '^@testing-library/react$': '<rootDir>/node_modules/@testing-library/react',
-    '^react-redux$': '<rootDir>/node_modules/react-redux',
-    '^react-router-dom$': '<rootDir>/node_modules/react-router-dom',
-    '^redux-mock-store$': '<rootDir>/node_modules/redux-mock-store',
-  },
-  moduleFileExtensions: ['js', 'jsx'],
 };
